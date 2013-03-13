44a28d808f9c3206bd508aec48df5259ab86bcec
Andy Toone <feersum.technology@gmail.com>
2013-03-13 14:26:42 +0000

    Test should not be sensitive to ordering of rights

--- src/test/java/org/atlasapi/remotesite/music/emipub/EmiPubProcessorTest.java
+++ src/test/java/org/atlasapi/remotesite/music/emipub/EmiPubProcessorTest.java
@@ -30,14 +30,27 @@ public class EmiPubProcessorTest {
                 capture.getAllValues().get(0).getCanonicalUri());
         assertEquals(1,
                 capture.getAllValues().get(0).getPeople().size());
-        assertEquals("Synchronisation Right:100.0,Performing Right:50.0,Material Change/Adaptation:100.0,Mechanical Right:100.0,Other:100.0",
-                capture.getAllValues().get(0).getVersions().iterator().next().getRestriction().getMessage());
-        
+        assertContainsListInAnyOrder(capture.getAllValues().get(0).getVersions().iterator().next().getRestriction().getMessage(), 
+        		new String[] {"Synchronisation Right:100.0","Performing Right:50.0","Material Change/Adaptation:100.0","Mechanical Right:100.0","Other:100.0"}, ",");
         assertEquals("http://emimusicpub.com/works/1380618",
                 capture.getAllValues().get(1).getCanonicalUri());
         assertEquals(2,
                 capture.getAllValues().get(1).getPeople().size());
-        assertEquals("Synchronisation Right:100.0,Performing Right:50.0,Material Change/Adaptation:100.0,Mechanical Right:100.0,Other:100.0",
-                capture.getAllValues().get(1).getVersions().iterator().next().getRestriction().getMessage());
+        assertContainsListInAnyOrder(capture.getAllValues().get(1).getVersions().iterator().next().getRestriction().getMessage(),
+        		new String[]{"Synchronisation Right:100.0","Performing Right:50.0","Material Change/Adaptation:100.0","Mechanical Right:100.0","Other:100.0"}, ",");
+    }
+    
+    private void assertContainsListInAnyOrder(String value, String[] expectedItems, String separator) {
+    	int expectedLength = 0;
+    	for( String expected: expectedItems) {
+    		if( expectedLength > 0 ) expectedLength += separator.length();
+    		expectedLength += expected.length();
+    		
+    		assertTrue("List should contain every item, missing "+expected, value.indexOf(expected) >= 0);
+    	}
+    	
+    	assertEquals("List should match total length of items", expectedLength, value.length());
+    	
+    	
     }
 }
